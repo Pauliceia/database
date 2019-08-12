68fce6d7a9c96ea87b846e0de4a38a2508db46d6
Rodrigo Mariano <rodmariano13@gmail.com>
2019-08-12 16:11:31 -0300

    Refactored code

--- script_to_fix_accents.py
+++ script_to_fix_accents.py
@@ -14,34 +14,6 @@ FOLDER_TO_SAVE_SHP = "streets_pilot_area_new"
 FILE_TO_SAVE_SHP = "streets_pilot_area.shp"
 
 
-# read file
-shapefile = gpd.read_file(DIR_ORIGINAL_SHP)
-
-# shapefile['name'] = shapefile['name'].astype(str)
-# shapefile['type'] = shapefile['type'].astype(str)
-
-shapefile.loc[shapefile['name'].isnull(), 'name'] = ""
-shapefile.loc[shapefile['type'].isnull(), 'type'] = ""
-# print(shapefile.name.apply(lambda x: len(x)).min())
-
-print(shapefile[shapefile.fid==142].name)
-shapefile['new_name'] = shapefile['name']
-
-# print(shapefile[shapefile['name'].str.startswith(shapefile['type'])].shape)
-
-lista = []
-
-for k, linha in shapefile.iterrows():
-    if not linha['name'].startswith(linha['type']):
-        lista.append(linha['fid'])
-
-shapefile.loc[shapefile['fid'].isin(lista), 'new_name'] = shapefile['type'] + ' '+shapefile['name']
-shapefile['name'] = shapefile['new_name']
-del shapefile['new_name']
-print(shapefile[shapefile.fid==142].name)
-
-
-
 acronyms = [
     {"old": " dr. ", "new": " doutor "},
     {"old": "dr ", "new": "doutor "},
@@ -58,28 +30,6 @@ def fix_acronyms(cell):
         cell = cell.replace(acronym["old"], acronym["new"])
 
     return cell
-        
-
-
-shapefile['name'] = shapefile.name.apply(fix_acronyms)
-# print(shapefile.loc[shapefile['name'].str.startswith("shapefile['type']"), 'name'].shape)
-# = shapefile['type'] + ' ' + shapefile['name']
-
-#shapefile['name'] = shapefile['type'] + ' ' + shapefile['name']
-saida = FOLDER_TO_SAVE_SHP + "/" + FILE_TO_SAVE_SHP
-print(saida)
-shapefile.to_file(saida)
-exit('teste')
-
-
-# print("shape.shape: ", shape.shape)
-
-# shape.plot()
-
-# print("shape.crs: ", shape.crs)
-
-
-# print("shape.head(15): \n", shape.head(15))
 
 
 # this function is not needed anymore, because when I save the Shapefile using geopandas, the accents are fixed
@@ -89,54 +39,42 @@ exit('teste')
 
 #     return attribute.encode('latin-1').decode('raw_unicode_escape').encode('latin-1').decode('utf-8')
 
+
 # shape['name'] = shape.name.apply(fix_accents)
 
+# read file
+shapefile = gpd.read_file(DIR_ORIGINAL_SHP)
 
-
-# shape_copy = shape.copy()
-
-for index, row in shapefile.iterrows():
-    # if there is some None attribute, then add an empty string instead
-    if row['name'] is None or row['type'] is None:
-        if row['name'] is None:
-            shapefile.at[index, 'name'] = ""
-        if row['type'] is None:
-            shapefile.at[index, 'type'] = ""
-        continue
-
-    # if "seguro porto" in row['name']:
-    #     print("\nrow['type']: ", row['type'])
-    #     print("row['name']: ", row['name'])
-
-    # if name does not start with its type, then add it as prefix
-    if not row['name'].startswith(row['type']):
-        new_name = row['type'] + " " + row['name']
-        # shapefile.at[index, 'name'] = new_name
-
-        shapefile.set_value(index, 'name', new_name)
-
-        # if "seguro porto" in row['name']:
-        #     print("row['type']: ", row['type'])
-        #     print("row['name']: ", row['name'])
-        #     print("new_name: ", new_name)
-    
-    # fix possible acronyms
-    shapefile.at[index, 'name'] = fix_acronyms(row['name'])
-
-    # print(row['type'], row['name'])
-
-
-
-
-
-
-
-
-
-
-
+# if there is NaN values, then replace them to empty string
+shapefile.loc[shapefile['name'].isnull(), 'name'] = ""
+shapefile.loc[shapefile['type'].isnull(), 'type'] = ""
 
 # print("shape.head(15): \n", shape.head(15))
+# print(shapefile[shapefile.fid==142].name)
+# print(shapefile[shapefile.fid==168].name)
+
+# create a new attribute based on name
+shapefile['new_name'] = shapefile['name']
+
+lista = []
+
+for k, linha in shapefile.iterrows():
+    if not linha['name'].startswith(linha['type']):
+        lista.append(linha['fid'])
+
+shapefile.loc[shapefile['fid'].isin(lista), 'new_name'] = shapefile['type'] + ' '+shapefile['name']
+
+# add the value of 'new_name' to 'name' and delete the old 'new_name'
+shapefile['name'] = shapefile['new_name']
+del shapefile['new_name']
+
+# print("shape.head(15): \n", shape.head(15))
+# print(shapefile[shapefile.fid==142].name)  
+# print(shapefile[shapefile.fid==168].name)  # empty name, but 'rua' is added as prefix
+
+# apply the 'fix_acronyms' function to each cell on my 'name' column
+shapefile['name'] = shapefile.name.apply(fix_acronyms)
+
 
 # remove unnecessary attributes
 if 'id_type' in shapefile:
