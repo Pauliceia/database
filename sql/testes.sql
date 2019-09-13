eede6c83b1d5c6501c77454e98e6a304ebdfa969
Rodrigo Mariano <rodmariano13@gmail.com>
2019-09-13 16:25:09 -0300

    Added testes.sql

---
+++ sql/testes.sql
@@ -0,0 +1,8 @@
+﻿-- verify if there is more than one street with the same name
+select ou.id, ou.name from streets_pilot_area ou
+where (
+	select count(*) 
+	from streets_pilot_area inr
+	where inr.name = ou.name
+) > 1
+ORDER BY ou.name, ou.id;
